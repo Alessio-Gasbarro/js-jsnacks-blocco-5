0e0b9feee98c1f913ab64825476a3d2eedd97568
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-04-02 17:59:20 +0200

    [DONE] Ex8 DONE

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studentBis = students.find(students => students.name === 'Marco Lanci');
+console.log(studentBis.class);
